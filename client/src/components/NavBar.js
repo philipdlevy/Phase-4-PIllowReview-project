94d887ec8d44aebc00ef2dc0c2c14bca369326c6
Philip Levy <philipdlevy@gmail.com>
2022-07-27 15:04:26 -0600

    css added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from "react"
+import {Route, Switch} from 'react-router-dom'
 
 function NavBar() {
   return (
     <div className='container'>
       <h1 className='text'>| Welcome to Phil's Fantastic Pillows |</h1>
+      <button class="button-25" role="button">SignIn/Up</button>
     </div>
   );
 }
