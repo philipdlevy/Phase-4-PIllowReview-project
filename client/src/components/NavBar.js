03ce884adc4444ad5ead4f4f1564ac9076173e1c
Philip Levy <philipdlevy@gmail.com>
2022-07-27 14:51:04 -0600

    Added more css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <></>
-  )
+    <div className='container'>
+      <h1 className='text'>| Welcome to Phil's Fantastic Pillows |</h1>
+    </div>
+  );
 }
 
 export default NavBar
