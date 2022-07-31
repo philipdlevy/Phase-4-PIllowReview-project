19868eb08a3f5f75f16785e1f15e56402929bc2d
Philip Levy <philipdlevy@gmail.com>
2022-07-31 12:26:55 -0600

    Added new styling

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -2,20 +2,26 @@ import React from 'react'
 
 function ContactUs() {
   return (
-    <div>
-        <h1>About</h1>
+    <div className='contactinfo'>
+        <h1 className='underline'>About</h1>
             <p>
-              Welcome to our fitness page! This is a place to learn more about exercises to improve your health and to share your knowledge!
+              Welcome to our pillow review page! This is a place to leave reviews for pillows that you have tried to give potential customers an idea of how the product works before purchase. 
             </p>
             <p>
-              You can click on the "MORE INFO" button to learn more about an exercise on the home page. 
+              You can leave reviews on items that you have tried and if the item doesn't exist on the pgae, select the add item button and add the new item and then leave a review for it! 
             </p>
             <p>
-              In the Add Exercise page, you can add a new exercise to share with others. 
+              You can  also see the items you have reviewed and the reviews that you have written
             </p>
             <p>
               Have fun!! 
             </p>
+            <br></br>
+
+        <h1 className='underline'>Contact us</h1>
+          <p>
+            Email: pillowreviewersonline@fakemail.com
+          </p>
     </div>
   )
 }
